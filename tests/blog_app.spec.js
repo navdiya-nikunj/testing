69eda3a2927e303ebe2b9b9f081ca8dd734d3e24
navdiya-nikunj <nikunjmnavdiya@gmail.com>
2024-10-09 13:36:58 +0530

    Exercise 5.18

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -9,4 +9,37 @@ describe('Blog app', () => {
         const locator = page.getByText("Submit");
         expect(locator).toBeVisible();
     })
+
+    describe('login', () => {
+
+        beforeEach(async ({ page, request }) => {
+            await request.post('http://localhost:3003/api/testing/reset');
+            await request.post('http://localhost:3003/api/users', {
+                data: {
+                    name: "Testt",
+                    username: 'root',
+                    password: 'root'
+                }
+            })
+        })
+
+
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByTestId('username').fill("root");
+            await page.getByTestId('password').fill("root");
+
+            const button = await page.getByRole('button', { name: 'Submit' }).click();
+            await expect(page.getByText("test is logged in")).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByTestId('username').fill("test");
+            await page.getByTestId('password').fill("root");
+
+            const button = await page.getByRole('button', { name: 'Submit' }).click();
+            await expect(page.getByText("Wrong username or password")).toBeVisible()
+        })
+    })
+
+
 })
